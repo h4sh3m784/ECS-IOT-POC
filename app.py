e5bed1b69a2aa10e0b8a42295ce2aab25af44218
Unknown <h4sh3m784@gmail.com>
2019-02-25 10:06:26 +0100

    Added serverless file and web service app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/handle', methods=['GET', 'POST'])
+def login():
+
+    data = {"status":"null"}
+
+    if request.method == 'POST':
+         data = {"status": "POST"}
+    else:
+         data = {"status": "GET"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
