77f7eee05e6c0f7a2240874b8d46de1706cd3207
Unknown <h4sh3m784@gmail.com>
2019-02-26 21:12:22 +0100

    fix

--- script.py
+++ script.py
@@ -8,6 +8,9 @@ relative_URI = os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI']
 
 url = ip + relative_URI
 
+print(ip)
+print(relative_URI)
+print(url)
 output = subprocess.check_output(['curl', url])
 
 print(output)
