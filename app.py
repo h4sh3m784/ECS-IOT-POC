1bdcef94ca67335af59af8c8933281124dd57222
Unknown <h4sh3m784@gmail.com>
2019-02-26 16:16:47 +0100

    fix

--- app.py
+++ app.py
@@ -8,22 +8,23 @@ import os
 
 app = Flask(__name__)
 
-host = "a29zo009haxq0r-ats.iot.us-east-1.amazonaws.com"
-rootCAPath = "root-CA.crt"
-port = 443
-
-myAWSIoTMQTTClient = AWSIoTMQTTClient(str(uuid.uuid4()), useWebsocket=True)
-myAWSIoTMQTTClient.configureEndpoint(host,port)
-myAWSIoTMQTTClient.configureCredentials(rootCAPath) 
-myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1,32,20)
-myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)
-myAWSIoTMQTTClient.configureDrainingFrequency(2)
-myAWSIoTMQTTClient.configureConnectDisconnectTimeout(25)
-myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)
 
 #Route
 @app.route('/device/<DeviceId>', methods=['GET', 'POST'])
 def PublishToIoT(DeviceId):
+    
+    host = "a29zo009haxq0r-ats.iot.us-east-1.amazonaws.com"
+    rootCAPath = "root-CA.crt"
+    port = 443
+
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(str(uuid.uuid4()), useWebsocket=True)
+    myAWSIoTMQTTClient.configureEndpoint(host,port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath) 
+    myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1,32,20)
+    myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)
+    myAWSIoTMQTTClient.configureDrainingFrequency(2)
+    myAWSIoTMQTTClient.configureConnectDisconnectTimeout(25)
+    myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)
 
     myAWSIoTMQTTClient.connect()
     
