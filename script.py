66ad7b2e7b524bf06a96332ef834db0a7760b3b3
Unknown <h4sh3m784@gmail.com>
2019-02-26 21:14:27 +0100

    fix

--- script.py
+++ script.py
@@ -2,13 +2,10 @@ import socket
 import subprocess
 import os
 
-ip = socket.gethostbyname(socket.gethostname())
-
 relative_URI = os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI']
 
-url = ip + relative_URI
+url = "169.254.170.2" + relative_URI
 
-print(ip)
 print(relative_URI)
 print(url)
 output = subprocess.check_output(['curl', url])
