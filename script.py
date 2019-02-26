c1a5592aca0fb2a34b3c5bb242b3782eb320ffd9
Unknown <h4sh3m784@gmail.com>
2019-02-26 21:28:04 +0100

    pre-script done

--- script.py
+++ script.py
@@ -7,7 +7,13 @@ relative_URI = os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI']
 url = "169.254.170.2" + relative_URI
 
 print(relative_URI)
+
 print(url)
+
 output = subprocess.check_output(['curl', url])
 
 print(output)
+
+os.environ["AWS_ACCESS_KEY_ID"] = output[b'AccessKeyId']
+os.environ["AWS_SECRET_ACCESS_KEY"] = output[b'SecretAccessKey']
+os.environ["AWS_SESSION_TOKEN"] = output[b'Token']
