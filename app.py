99e92ffd0754a696789a5bd4af317be8e94c65f5
Unknown <h4sh3m784@gmail.com>
2019-02-27 11:36:30 +0100

    Iot

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import json
 import uuid
 import os
-import socket
 import subprocess
+import time
 
+#Tokens need to be refreshed every X amount of hour
+#So you need to run this script again..
 relative_URI = os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI']
 
 url = "169.254.170.2" + relative_URI
@@ -22,33 +24,56 @@ os.environ["AWS_SESSION_TOKEN"] = data['Token']
 
 app = Flask(__name__)
 
+host = "a29zo009haxq0r-ats.iot.us-east-1.amazonaws.com"
+rootCAPath = "root-CA.crt"
+sub_topic = "WebInterface/iot/sub"
+port = 443
+
+data = {}
+
+myAWSIoTMQTTClient = AWSIoTMQTTClient(str(uuid.uuid4()), useWebsocket=True)
+myAWSIoTMQTTClient.configureEndpoint(host,port)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1,32,20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)
+myAWSIoTMQTTClient.configureDrainingFrequency(2)
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(25)
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)
+myAWSIoTMQTTClient.connect()
+
+def Callback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+    data['Message'] = message
+    Callback.has_been_called = True
+
+myAWSIoTMQTTClient.subscribe(sub_topic, 0, Callback)
+
 #Route
 @app.route('/device/<DeviceId>', methods=['GET', 'POST'])
 def PublishToIoT(DeviceId):
 
-    host = "a29zo009haxq0r-ats.iot.us-east-1.amazonaws.com"
-    rootCAPath = "root-CA.crt"
-    port = 443
+    pub_topic = "Webinterface/iot/pub/" + DeviceId
 
-    myAWSIoTMQTTClient = AWSIoTMQTTClient(str(uuid.uuid4()), useWebsocket=True)
-    myAWSIoTMQTTClient.configureEndpoint(host,port)
-    myAWSIoTMQTTClient.configureCredentials(rootCAPath) 
-    myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1,32,20)
-    myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)
-    myAWSIoTMQTTClient.configureDrainingFrequency(2)
-    myAWSIoTMQTTClient.configureConnectDisconnectTimeout(25)
-    myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)
+    global data
 
-    myAWSIoTMQTTClient.connect()
-    
-    data = {}
     if request.method == 'GET':
          data = {"status": "GET"}
+
     data = jsonify(data)
+    
     message = {}
     message['DeviceId'] = DeviceId
     message = json.dumps(message)
-    myAWSIoTMQTTClient.publish("weird/topic", message,0)
+
+    myAWSIoTMQTTClient.publish(pub_topic, message,0)
+
+    while not Callback.has_been_called:
+        time.sleep(0.1)
+
     return data
 
 if __name__ == '__main__':
--- script.py
+++ script.py
@@ -1,1 +1,11 @@
-print("kek")
+def example():
+    example.has_been_called = True
+    pass
+example.has_been_called = False
+
+
+# example()
+
+#Actual Code!:
+if example.has_been_called:
+   print("foo bar")
