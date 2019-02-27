9eb9c9e5453e35b70f01d2d8db12f9de69473716
Unknown <h4sh3m784@gmail.com>
2019-02-27 10:25:26 +0100

    comments added, need to refresh tokens

--- app.py
+++ app.py
@@ -12,22 +12,13 @@ relative_URI = os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI']
 
 url = "169.254.170.2" + relative_URI
 
-print(relative_URI)
-
-print(url)
-
 output = subprocess.check_output(['curl', url])
-
-print(output)
 
 data = json.loads(output)
 
 os.environ["AWS_ACCESS_KEY_ID"] = data['AccessKeyId']
-print(data['AccessKeyId'])
 os.environ["AWS_SECRET_ACCESS_KEY"] = data['SecretAccessKey']
-print(data['SecretAccessKey'])
 os.environ["AWS_SESSION_TOKEN"] = data['Token']
-print(data['Token'])
 
 app = Flask(__name__)
 
