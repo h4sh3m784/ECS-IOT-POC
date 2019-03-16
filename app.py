8566be0170632212c59a7c2822d96ef9858e716d
Unknown <h4sh3m784@gmail.com>
2019-03-16 21:29:52 +0100

    kek

--- app.py
+++ app.py
@@ -110,4 +110,4 @@ def response_device(device_id):
     return '{"Status": "200"}'
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=80,threaded=True)
