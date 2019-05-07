3dc8bf7e5996d8f1eec24b30995abbf2ba00f850
Unknown <h4sh3m784@gmail.com>
2019-05-07 13:38:30 +0200

    fuck

--- preconfig.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "-------------PRE-CONFIG---------------"
-
-python /src/run.py
--- serverless/handler.py
+++
@@ -1,8 +0,0 @@
-import json
-from botocore.vendored import requests
-
-def lambda_handler(event, context):
-    info = event['MessageInfo']
-    endpoint = "http://" + info['EndPoint'] #Get the web-server endpoint
-    req = requests.post(endpoint, json.dumps(event)) #Send POST request to endpoint
-    return req.status_code
