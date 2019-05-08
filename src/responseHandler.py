b691e21c088afccbba9eaed84c1ae88b969f09ff
Unknown <h4sh3m784@gmail.com>
2019-05-08 14:59:46 +0200

    MOE VAN JEZELF..

--- src/responseHandler.py
+++ src/responseHandler.py
@@ -5,7 +5,7 @@ from flask import Blueprint
 from flask import request
 import logging
 
-logging.basicConfig(level=logging.BASIC_FORMAT)
+logging.basicConfig(level=logging.DEBUG)
 
 responseView = Blueprint('responseView', __name__)
 
@@ -14,8 +14,10 @@ responses = dict()
 @responseView.route('/lambda-response', methods=['POST'])
 def responseDevice():
     response = json.loads(request.data)
+    logging.debug(response)
     key = response['RequestId']
     if key in requestHandler.events: #Check if event hasn't time-out
+        logging.debug("we in..")
         responses[key] = response #Save response
         requestHandler.events[key].set() #Set the waiting event
     return '{"status": "200"}'
